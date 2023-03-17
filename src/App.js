60ad0d2855c3a6c43f292e0e6c437e75557a726e
Sameer Rana <Sameer.r@antino.io>
2023-03-17 11:03:54 +0530

    single query UI done

--- src/App.js
+++ src/App.js
@@ -3,10 +3,9 @@ import Login from './pages/Login';
 import SingleQuery from './components/SingleQuery';
 function App() {
   return (
-    <div>
-      {/* <Login/> */}
+      <>
       <SingleQuery />
-    </div>
+      </>
     // <h1 className="text-3xl font-bold underline">
     //   Hello world!
     // </h1>
--- src/components/SingleQuery.js
+++ src/components/SingleQuery.js
@@ -1,8 +1,9 @@
 import React from "react";
 const SingleQuery = () => {
   return (
-    <div className="menu flex mt-20 ml-20 mr-20 space-x-5">
-      <div className="leftBar w-2/3 h-2/4 bg-slate-600">
+    <div className="bg-gray-200 h-screen w-full m-0 flex justify-center items-center">
+    <div className="menu flex  space-x-5 h-auto w-[60%]">
+      <div className="leftBar w-2/3 h-2/4 bg-white rounded-md">
         <div className="top flex p-4">
           <div className="flex">
             <img
@@ -10,49 +11,44 @@ const SingleQuery = () => {
               class="w-20 rounded-full"
               alt="Avatar"
             />
-            <div className="UserCred ml-4 text-white">
+            <div className="UserCred ml-4 text-black">
               <p className="text-lg font-extrabold">Chaman Singh</p>
-              <p className="text-lg ">Email</p>
+              <p className="text-lg text-slate-800">Email</p>
               <p className="text-lg font-semibold">Chamansingh@gmail.com</p>
             </div>
           </div>
         </div>
-        <div className="UserDetails mt-10 ml-10">
-          <h1 className="text-lg font-semibold text-white">User Details</h1>
-          <hr className="" />
-          <div className="">
-            <table class="table">
-              <thead>
-                <tr>
-                  <th>Song</th>
-                  <th>Artist</th>
-                  <th>Year</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td>The Sliding Mr. Bones (Next Stop, Pottersville)</td>
-                  <td>Malcolm Lockyer</td>
-                  <td>1961</td>
-                </tr>
-                <tr>
-                  <td>Witchy Woman</td>
-                  <td>The Eagles</td>
-                  <td>1972</td>
-                </tr>
-                <tr>
-                  <td>Shining Star</td>
-                  <td>Earth, Wind, and Fire</td>
-                  <td>1975</td>
-                </tr>
-              </tbody>
-            </table>
+        <div className="UserDetails mx-auto w-[85%] mt-12 ">
+          <h1 className="text-lg font-semibold text-black mb-5">User Details</h1>
+          {/* <hr class="h-1 ml-0 mt-0 bg-slate-800 text-white border-0 rounded md:my-6 "></hr> */}
+          <div className="flex flex-col   mb-10 rounded-md space-y-2">
+            <div className="flex justify-between w-full p-2 mb-1 bg-slate-400">
+              <p className="text-xl">D.O.B</p>
+              <p className="text-xl">12/12/2000</p>
+            </div>
+            <div className="flex justify-between w-full p-2">
+              <p className="text-xl">Gender</p>
+              <p className="text-xl">Male</p>
+            </div>
+            <div className="flex justify-between w-full p-2  bg-slate-400">
+              <p className="text-xl">ID</p>
+              <p className="text-xl">01596202819</p>
+            </div>
+            <div className="flex justify-between w-full p-2">
+              <p className="text-xl">Nationality</p>
+              <p className="text-xl">Indian</p>
+            </div>
+            <div className="flex justify-between w-full p-2  bg-slate-400">
+              <p className="text-xl">Bank Name</p>
+              <p className="text-xl">SBI</p>
+            </div>
           </div>
         </div>
       </div>
       <div className="RightBar w-1/4 h-1/4 bg-indigo-600">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Non, et!
       </div>
+    </div>
     </div>
   );
 };
